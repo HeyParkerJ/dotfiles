2e4368d4e210980f0b265c06a2c92b49c3e39f17
Parker Johnson <mrstormthebeach@gmail.com>
2020-02-11 06:21:54 -0700

    remove compiled .el files from /emacs/ dir

--- emacs/org-mode.work.el
+++
@@ -1,7 +0,0 @@
-(setq org-capture-templates '(("t" "Todo [inbox]" entry
-                               (file+headline "~/org/inbox.org" "Tasks")
-                                 "* TODO %i%?")
-                                 ))
-
-(setq org-agenda-files
-      '("~/org/work.org" "~/org/inbox.org"))
