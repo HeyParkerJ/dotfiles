902aabeeae3fd7db4cc2b0e5bc986d00c073d494
Parker Johnson <mrstormthebeach@gmail.com>
2020-12-30 23:30:31 -0700

    remove config backups, update config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -31,6 +31,21 @@
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+;; org roam config
+;; (use-package org-roam
+;;       :ensure t
+;;       :hook
+;;       (after-init . org-roam-mode)
+;;       :custom
+;;       (org-roam-directory "~/org")
+;;       :bind (:map org-roam-mode-map
+;;               (("C-c n l" . org-roam)
+;;                ("C-c n f" . org-roam-find-file)
+;;                ("C-c n g" . org-roam-graph))
+;;               :map org-mode-map
+;;               (("C-c n i" . org-roam-insert))
+;;               (("C-c n I" . org-roam-insert-immediate))))
+
   ;; System information
   (defun my/laptop-p ()
     (equal (system-name) "Parkers-MBP"))
--- .doom.d/init.el
+++ .doom.d/init.el
@@ -141,6 +141,7 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        org               ; organize your plain life in plain text
+       +roam               ; org roam
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
